6fd20decab0f80bc3794aa2cf1e57f5dd7824e4b
Krislucas <krislucasthomas@gmail.com>
2024-02-22 23:06:22 +0530

    signup page (unstyled)

--- src/components/SignupPage/Signuup.jsx
+++ src/components/SignupPage/Signuup.jsx
@@ -1,8 +1,44 @@
+import { Link } from "react-router-dom";
+import "./Signup.css";
+
 let Signup = ()=>{
     return(
         <div className="signup-page">
 
-            <h1>This is signup Page</h1>
+            <div className="signup-logo">
+                <img src="./photos/logo.png" alt="signup-page-logo" className="signup-page-logo"/>  
+            </div>
+            <div className="child-row">
+                <input type="text" name="name"/>
+                <select name="interest" id="">
+                    <option value="agriculture">Agriculture</option>
+                    <option value="finance">Finance</option>
+                    <option value="finance">Technology</option>
+                    <option value="finance">Research</option>
+                    <option value="finance">Medical</option>
+                </select>
+            </div>
+            <div className="child-row">
+                <input type="text" name="email" placeholder="Email"/>
+                <input type="text" name="password" placeholder="password"/>
+            </div>
+            <div className="child-row">
+                <input type="text" name="city" placeholder="City"/>
+                <select name="role" id="">
+                    <option value="investor">Investor</option>
+                    <option value="raiser">Fund Raiser</option>
+                </select>
+            </div>
+            <div className="child">
+                <textarea name="description" id="" ></textarea>
+            </div>
+            <div className="child">
+                <button type="submit" className="signup_button">Sign In</button>
+            </div>
+            <div className="child">
+                <p>Already Signed Up ? <span id ="color_primary">Login</span> </p>
+
+            </div>
         
         
         </div>
