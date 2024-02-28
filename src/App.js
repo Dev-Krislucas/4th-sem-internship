b4df0fb19bf44087b85a213bb6167100c8e6c601
Krislucas <krislucasthomas@gmail.com>
2024-02-28 12:39:51 +0530

    Added a login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Entry from "./components/ENTRY/Entry";
 import Login from "./components/Login/login";
+import Signup from "./components/SignupPage/Signuup";
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
 
         <Route path="/" element={<Entry/>} />
         <Route path="/login" element={<Login />} />
-        {/* <Route to="" element={} /> */}
+        <Route path="/signup" element={<Signup />} />
         {/* <Route to="" element={} /> */}
         {/* <Route to="" element={} /> */}
         {/* <Route to="" element={} /> */}
--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -1,8 +1,29 @@
+import "./login.css";
+import { Link } from "react-router-dom";
+
 let Login = ()=>{
     return(
-        <>
-            <h1>This is login page</h1>
-        </>
+        <div className="login-page">
+
+        <div className="signup-logo">
+            <img src="./photos/logo.png" alt="signup-page-logo" className="login-page-logo"/>  
+        </div>
+        
+        <div className="child-column">
+            <input type="text" name="email" placeholder="Email" className="login-text"/>
+            <input type="text" name="password" placeholder="Password" className="login-text"/>
+        </div>
+        
+        <div className="child">
+            <button type="submit" className="login_button">Sign In</button>
+        </div>
+        <div className="child">
+            <p>Don't have an account ? <Link to="/signup" ><span id ="color_primary">Signup</span></Link> </p>
+
+        </div>
+    
+    
+    </div>
     )
 
 
